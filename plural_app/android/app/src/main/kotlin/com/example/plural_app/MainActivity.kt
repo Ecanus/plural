506fed373c53522a5e5a7e76646edcc62e99ef7b
Dedie <dolente@live.com>
2024-09-05 17:30:00 -0400

    plural app first major push

---
+++ plural_app/android/app/src/main/kotlin/com/example/plural_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plural_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ plural_app/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:plural_app/src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
