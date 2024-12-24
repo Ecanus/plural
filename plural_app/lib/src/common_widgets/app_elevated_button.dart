14b931c6aed97a5dce727121b62e34fd352b1283
Dedie <dolente@live.com>
2024-12-24 13:19:59 -0500

    feat(core): update landing page styling
    Update landing page buttons and input widgets to match application theme
    and stylings.

--- plural_app/lib/src/common_widgets/app_elevated_button.dart
+++ plural_app/lib/src/common_widgets/app_elevated_button.dart
@@ -7,22 +7,24 @@ import 'package:plural_app/src/constants/app_sizes.dart';
 class AppElevatedButton extends StatelessWidget {
   const AppElevatedButton({
     required this.callback,
+    this.icon,
     required this.label,
-    this.positionalArguments,
     this.namedArguments,
+    this.positionalArguments,
   });
 
   final Function callback;
+  final IconData? icon;
   final String label;
-  final List<dynamic>? positionalArguments;
   final Map<Symbol, dynamic>? namedArguments;
+  final List<dynamic>? positionalArguments;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       height: AppButtonSizes.s50,
-      child: ElevatedButton(
+      child: ElevatedButton.icon(
         onPressed: () => Function.apply(callback, positionalArguments, namedArguments),
         style: ElevatedButton.styleFrom(
           backgroundColor: Theme.of(context).colorScheme.primary,
@@ -31,7 +33,13 @@ class AppElevatedButton extends StatelessWidget {
             borderRadius: BorderRadius.circular(AppBorderRadii.r5),
           )
         ),
-        child: Text(
+        icon: icon != null ?
+          Icon(
+            icon,
+            color: Theme.of(context).colorScheme.onPrimary,
+          )
+          : null,
+        label: Text(
           label,
           style: Theme.of(context).textTheme.labelLarge,
         ),
