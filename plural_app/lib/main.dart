e1dabec6ffe871eacd943c8950340299847a6972
Dedie <dolente@live.com>
2025-02-06 10:20:53 -0500

    fixup! fixup! feat(core): add garden timeline tilebackground

--- plural_app/lib/main.dart
+++ plural_app/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plural_app/src/app.dart';
 
-import 'package:plural_app/src/features/authentication/data/auth_repository.dart';
-
 void main() async{
-  await login("user2@test.com", "pickles2");
   runApp(MyApp());
 }
