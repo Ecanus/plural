e7a06c684e735a294f55b902c17726a78289fe4a
Dedie <dolente@live.com>
2025-04-14 16:43:05 -0400

    fix: add Hms to deadline date filtration
    Fixes #21

--- plural_app/lib/src/constants/formats.dart
+++ plural_app/lib/src/constants/formats.dart
@@ -1,4 +1,5 @@
 class Formats {
   static const dateYMMdd = "y-MM-dd";
+  static const dateYMMddHms = "y-MM-dd H:mm:ss";
   static const dateInputMMddY = "MM/dd/y";
 }
--- plural_app/lib/src/utils/app_state.dart
+++ plural_app/lib/src/utils/app_state.dart
@@ -58,7 +58,7 @@ class AppState with ChangeNotifier {
   }
 
   Future<List<Ask>> getTimelineAsks() async {
-    var nowString = DateFormat(Formats.dateYMMdd).format(DateTime.now());
+    var nowString = DateFormat(Formats.dateYMMddHms).format(DateTime.now());
 
     // Asks with target met, or deadlineDate passed are filtered out.
     var filterString = ""
