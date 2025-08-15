d25c9c661016ede731c03516206a46db0db8877f
Dedie <dolente@live.com>
2025-08-14 21:49:10 -0400

    fixup! test: update all tests to use factories

--- plural_app/test/features/gardens/presentation/landing_page_gardens_tab_test.dart
+++ plural_app/test/features/gardens/presentation/landing_page_gardens_tab_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:plural_app/src/constants/pocketbase.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 // Constants
